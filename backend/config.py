0359840443378c3f690941368156fc77e16a7f44
Gavin Heatherington <gavinheatherington@gmail.com>
2024-10-26 11:16:55 -0400

    CORS

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,7 @@
 from flask import Flask 
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-
 app: Flask = Flask(__name__)
-CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "sqlite:///mydatabase.db"  # "mysql:" + os.env("DB_USERNAME") + os.env("DB_PASSWORD")  os.env("DB_LOCATION")
